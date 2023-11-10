163ac6794670822df836e3913bec420783adb998
leviiloviee <leviiloviee@gmail.com>
2023-11-10 22:05:57 +0700

    Change Emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -34,6 +34,16 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
+(use-package org-roam
+  :ensure t
+  :init
+  (setq org-roam-v2-ack t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n i" . org-roam-node-insert))
+  :config
+  (org-roam-setup))
+
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(fullscreen . fullheight))
 
