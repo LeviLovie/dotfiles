c002648edc8ab52bc0b32cf9d77c41089c88d70f
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2025-03-02 10:38:38 +0700

    Add window keybinds

--- nvim/lua/config/keys.lua
+++ nvim/lua/config/keys.lua
@@ -1,11 +1,23 @@
 vim.g.mapleader = " "
 local keymap = vim.keymap
 
-keymap.set('n', "<leader>sl", ":call vimspector#Launch()<cr>")
-keymap.set('n', "<leader>sr", ":call vimspector#Reset()<cr>")
-keymap.set('n', "<leader>ss", ":call vimspector#StepOver()<cr>")
-keymap.set('n', "<leader>su", ":call vimspector#StepOut()<cr>")
-keymap.set('n', "<leader>si", ":call vimspector#StepInto()<cr>")
-keymap.set('n', "<leader>sb", ":call vimspector#ToggleBreakpoint()<cr>")
-keymap.set('n', "<leader>sa", ":call vimspector#AddWatch()<cr>")
-keymap.set('n', "<leader>se", ":call vimspector#Evaluate()<cr>")
+-- <leader>g{hjkl} to move between windows
+keymap.set('n', "<leader>gh", "<C-w>h")
+keymap.set('n', "<leader>gj", "<C-w>j")
+keymap.set('n', "<leader>gk", "<C-w>k")
+keymap.set('n', "<leader>gl", "<C-w>l")
+
+-- <leader>g{-|} to split windows
+keymap.set('n', "<leader>g-", "<C-w>s")
+keymap.set('n', "<leader>g|", "<C-w>v")
+
+-- <Leader>g{HJKL} to resize windows
+keymap.set('n', "<leader>gH", "<C-w><")
+keymap.set('n', "<leader>gJ", "<C-w>-")
+keymap.set('n', "<leader>gK", "<C-w>+")
+keymap.set('n', "<leader>gL", "<C-w>>")
+
+-- <leader>gx to close window
+-- <leader>gX to close all windows except current
+keymap.set('n', "<leader>gx", "<C-w>c")
+keymap.set('n', "<leader>gc", "<C-w>o")
--- nvim/lua/plugins/vimspector.lua
+++ nvim/lua/plugins/vimspector.lua
@@ -3,6 +3,14 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     lazy = false,
     config = function()
-        vim.g.vimspector_enable_mappings = 'HUMAN'
+        local keymap = vim.keymap
+        keymap.set('n', "<leader>sl", ":call vimspector#Launch()<cr>")
+        keymap.set('n', "<leader>sr", ":call vimspector#Reset()<cr>")
+        keymap.set('n', "<leader>ss", ":call vimspector#StepOver()<cr>")
+        keymap.set('n', "<leader>su", ":call vimspector#StepOut()<cr>")
+        keymap.set('n', "<leader>si", ":call vimspector#StepInto()<cr>")
+        keymap.set('n', "<leader>sb", ":call vimspector#ToggleBreakpoint()<cr>")
+        keymap.set('n', "<leader>sa", ":call vimspector#AddWatch()<cr>")
+        keymap.set('n', "<leader>se", ":call vimspector#Evaluate()<cr>")
     end
 }
